4967813978230f7e43693b5bccd6d12b8179093f
thevortigaunt <romelolucas@gmail.com>
2018-02-20 14:34:14 -0500

    added kick box method

--- src/org/usfirst/frc/team1761/robot/commands/KickBox.java
+++ src/org/usfirst/frc/team1761/robot/commands/KickBox.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1761.robot.commands;
 
 import org.usfirst.frc.team1761.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -21,6 +22,17 @@ public class KickBox extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	//arms must be retracted
+    	Robot.m_fourbar.retractArms();
+    	//keep arms retracted while we fire kicker
+    	Robot.m_fourbar.kick();
+    	//wait about .5 seconds
+    	Timer.delay(.5);
+    	//release arms
+    	Robot.m_fourbar.extendArms();    	
+    	//retract kicker
+    	Robot.m_fourbar.windup();
+       	
     }
 
     // Make this return true when this Command no longer needs to run execute()
