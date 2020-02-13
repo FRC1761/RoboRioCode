1732154ee10ffe46b8da35b8b349fc84d88959b4
Kevin Ankiewicz <ankiewiczk@lynnschools.org>
2020-02-13 16:25:41 -0500

    moving Constants (let's try this again...) and added comments and game controller addresses

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,29 @@
 package frc.robot;
 
 public class Constants {
+  //DriveTrain Addresses on CAN bus
   public static final int FrontLeftVictor = 1;
   public static final int FrontRightVictor = 2;
   public static final int RearLeftVictor = 3;
   public static final int RearRightVictor = 4;
+
+  //Shotoer addresses CAN bus
   public static final int ShooterLeftVictorSP = 10;
   public static final int ShooterRightVictorSP = 20;
+
+  // Intake /Conveyor/ Shooter addresses (PWM)
   public static final int IntakeSP = 0;
   public static final int ConveryorSP = 1;
   public static final int ShooterTargeterSP = 2;
   public static final int climberSP = 3;
-  //public static final int ShooeterTargeterEn = 
+  
+  //Encoder addresses not needed because of using hardware in the loop
+  //public static final int ShooterTargeterEn = 
   public static final int ShooterCameras = 0;
   public static final int IntakeCamera = 1;
+
+  //GameController Addresses
+  public static final int DriverControllerPort 0;
+  public static final int ShooterControllerPort 2;
 
 }
