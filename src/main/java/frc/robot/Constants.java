1582f99b359c761d4d10c4db06cd0c0e770abfac
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-02-12 10:21:15 -0500

    Create Constants.java & populate
    Created Constants.java and populated with 4 Talons & 2 Joysticks

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    
+    public static final String bestProgrammer = "Aleksandr Usanov";
+    
+    public static final int frontLeftTalon   = 7;
+    public static final int frontRightTalon  = 13;
+    public static final int rearLeftTalon    = 40;
+    public static final int rearRightTalon   = 10;
+
+    public static final int leftJoystickPort    = 0;
+    public static final int rightJoystickPort   = 1;
+}
