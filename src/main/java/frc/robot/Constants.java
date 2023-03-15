59915ebc2ca8828c7c094a03c1b42a0ba57f7c19
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-03-15 13:19:16 -0400

    Update Constants with Max's
    and tabs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,13 @@ public class Constants {
     
     public static final String bestProgrammer = "Aleksandr Usanov";
     
-    public static final int frontLeftTalon   = 7;
-    public static final int frontRightTalon  = 13;
-    public static final int rearLeftTalon    = 40;
-    public static final int rearRightTalon   = 10;
+    public static final int frontLeftTalon		= 7;
+    public static final int frontRightTalon		= 13;
+    public static final int rearLeftTalon		= 40;
+    public static final int rearRightTalon		= 10;
 
-    public static final int xboxPort         = 0;
+    public static final int elevatorMax			= 0;
+    public static final int extensionMax		= 0;
+
+    public static final int xboxPort			= 0;
 }
