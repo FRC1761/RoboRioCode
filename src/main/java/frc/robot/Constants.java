6aef118fb4e8799222757dbeb6dc04d7325c5b9d
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-03-17 12:14:21 -0400

    Update Constants.java
    phCanAddress, double solenoid ports, sparks, oh my!
    
    Co-Authored-By: Kevin D Ankiewicz <ankiewiczk@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,11 +9,23 @@ public class Constants {
     public static final int rearLeftTalon		= 40;
     public static final int rearRightTalon		= 10;
 
+    /*
     public static final int elevatorMax			= 0;
     public static final int extensionMax		= 0;
 
-	public static final int left_dS				= 0;
-	public static final int right_dS			= 1;
+    public static final int elevatorMin			= 0;
+    public static final int extensionMin		= 0;
+    /**/
+   //Solenoids port numbers
+    public static final int phCanAddress        = 20;
+
+	public static final int right_dS_port1      = 0;
+    public static final int right_dS_port2		= 1;
+	public static final int left_dS_port1		= 2;
+	public static final int left_dS_port2		= 3;
+
+    public static final int elevatorSpark       = 9;
+    public static final int extensionSpark      = 1;
 
     public static final int xboxPort			= 0;
 }
