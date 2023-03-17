e40076676ba4d2ffc2370afe29d1124d2f32dcef
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-03-17 17:31:57 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,40 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.cameraserver.*;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.subsystems.ArcadeDrive;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.CloseGrabber;
+import frc.robot.commands.OpenGrabber;
+import frc.robot.subsystems.ArcadeDrive;
+import frc.robot.subsystems.Grabber;
 
 public class Robot extends TimedRobot {
-  private ArcadeDrive arcadeDrive = new ArcadeDrive();
+  public static final Grabber Grabber = new Grabber();
+  public static final ArcadeDrive arcadeDrive = new ArcadeDrive();
+  private CommandXboxController gamepad1 = new CommandXboxController(Constants.xboxPort);
+	//private CommandXboxController gamepad2 = new CommandXboxController(1);
+
+  @Override
+  public void robotInit(){
+    UsbCamera frontCamera;
+		frontCamera = CameraServer.startAutomaticCapture(0);
+		frontCamera.setFPS(30);
+		frontCamera.setResolution(160,120);
+
+    Trigger rightBumpah = gamepad1.rightBumper(); // Creates a new Trigger object for the `X` button on exampleCommandController
+    rightBumpah.onTrue(new OpenGrabber());
+
+    Trigger leftBumpah = gamepad1.leftBumper(); // Creates a new Trigger object for the `X` button on exampleCommandController
+    leftBumpah.onTrue(new CloseGrabber());
+
+  }
 
   @Override
   public void teleopPeriodic() {
