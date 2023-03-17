a91f4e0ad29a4019a43d4635061d817dc83ed07d
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-03-17 12:12:29 -0400

    Create two commands to use Grabber subsys. functions
    Co-Authored-By: Kevin D Ankiewicz <ankiewiczk@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/CloseGrabber.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class CloseGrabber extends InstantCommand {
+  public CloseGrabber() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.Grabber);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Robot.Grabber.closeGrabber();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
