8861dccc7c5fd010b632aeca85d90e1bb16d8a03
ankiewiczk <ankiewiczk@lynnschools.org>
2025-01-13 16:17:11 -0500

    updated CAN bus addresses

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,13 +65,13 @@ public final class Constants {
     //TODO set ID's for all motor controllers.
     public static final int kFrontLeftDrivingCanId = 1;
     public static final int kRearLeftDrivingCanId = 3;
-    public static final int kFrontRightDrivingCanId = 2;
-    public static final int kRearRightDrivingCanId = 4;
+    public static final int kFrontRightDrivingCanId = 5;
+    public static final int kRearRightDrivingCanId = 7;
 
-    public static final int kFrontLeftTurningCanId = 5;
-    public static final int kRearLeftTurningCanId = 7;
-    public static final int kFrontRightTurningCanId = 62;
-    public static final int kRearRightTurningCanId = 28;
+    public static final int kFrontLeftTurningCanId = 2;
+    public static final int kRearLeftTurningCanId = 4;
+    public static final int kFrontRightTurningCanId = 6;
+    public static final int kRearRightTurningCanId = 8;
     public static final boolean kGyroReversed = true;    
   }
   
