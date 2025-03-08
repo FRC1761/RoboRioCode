b92fe673c16e722ba27c83ee0da0d98bd73c844b
ankiewiczk <ankiewiczk@lynnschools.org>
2025-03-07 21:47:23 -0500

    update PhotonLib version

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -57,6 +57,6 @@ public class Shooter extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    RobotPreferences.setShooterSpeeds(leftEncoder.getVelocity(),rightEncoder.getVelocity());
+    RobotPreferences.setShooterSpeeds(leftEncoder.getVelocity(),rightEncoder.getVelocity()/**/);
   }
 }
