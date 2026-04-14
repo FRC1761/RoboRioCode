07c10777061d11e8fb52304bb877648d7d3e6707
FuenteKA <keilafuentes599@gmail.com>
2026-04-14 00:14:11 -0400

    trying to add shooter to autonomous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,7 +80,7 @@ public class RobotContainer {
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
-
+    /**/
     m_driverController.start()
         .onTrue(new InstantCommand(
             () -> m_robotDrive.zeroHeading(),
@@ -195,9 +195,9 @@ public class RobotContainer {
         // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
         // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(.2, 0), new Translation2d(.4, 0)),
+        List.of(new Translation2d(-.6, .0), new Translation2d(-1.2, 0)),
         // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(0, 0, new Rotation2d(0)),
+        new Pose2d(-2.0, 0, new Rotation2d(0)),
         config);
 
     var thetaController = new ProfiledPIDController(
@@ -220,7 +220,13 @@ public class RobotContainer {
     m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
 
     // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, true));
+    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, true))
+             .andThen(new FunctionalCommand(
+                () -> {},
+                () -> {m_Shooter.shootFar();} ,
+                (interrupted) -> {m_Shooter.stop();},
+                () -> {return false;},
+                m_Shooter)).withTimeout(5).andThen(() -> {m_Shooter.stop();});
   }
 }
  
